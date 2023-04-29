bb906aa57f64abf9676a250a3bf204cbc298339a
Caio Petrucci Rosa <caiopetruccirosa@gmail.com>
2023-04-29 17:18:11 -0300

    finished user sign in implementation

--- server/src/utils/errors.ts
+++ server/src/utils/errors.ts
@@ -1,7 +1,15 @@
-const USER_ALREADY_EXISTS = "user already exists"
-const DATABASE_ERROR = "an error has occured when communicating with database"
+const USER_ALREADY_EXISTS = "[BAD REQUEST] user already exists"
+const INVALID_USERNAME = "[BAD REQUEST] invalid value for username"
+const INVALID_NICKNAME = "[BAD REQUEST] invalid value for nickname"
+const INVALID_EMAIL = "[BAD REQUEST] invalid value for email"
+const INVALID_PASSWORD = "[BAD REQUEST] invalid value for password"
+const DATABASE_ERROR = "[INTERNAL SERVER ERROR] an error has occured when communicating with database"
 
 export default {
     USER_ALREADY_EXISTS,
+    INVALID_USERNAME,
+    INVALID_NICKNAME,
+    INVALID_EMAIL,
+    INVALID_PASSWORD,
     DATABASE_ERROR
 }
