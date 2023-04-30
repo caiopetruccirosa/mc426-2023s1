14f5022d90eadce778fc28a9ae61994f30f3330d
Luan Fazolin <182236@dac.unicamp.br>
2023-04-30 17:42:27 -0300

    Change post's username to poster_username

--- db/sql/02-post.sql
+++ db/sql/02-post.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS post (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(255) REFERENCES users(username),
+    poster_username VARCHAR(255) REFERENCES users(username),
     date DATE,
     title VARCHAR(255),
     content TEXT
