081f23bf664578bbe7cff3543e961cd0a51d1c6a
Luan Fazolin <182236@dac.unicamp.br>
2023-04-30 17:41:54 -0300

    Add salt to user table

--- db/sql/01-user.sql
+++ db/sql/01-user.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS users (
     nickname VARCHAR(255),
     password VARCHAR(255),
     email VARCHAR(255),
-    role VARCHAR(255)
+    role VARCHAR(255),
+    salt VARCHAR(255)
 );
