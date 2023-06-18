0e24fc2ee61ff4b1a6cea1e85e922cbd32070ec8
Luan Fazolin <182236@dac.unicamp.br>
2023-06-18 16:37:47 -0300

    Remove dead code from MC322

--- client/src/MC322.js
+++ client/src/MC322.js
@@ -1,5 +1,4 @@
-import axios from "axios";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import UserContext from "./UserContext";
 
 function MC322() {
